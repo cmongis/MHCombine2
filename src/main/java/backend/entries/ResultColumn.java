1ab0cd0bf15d05f8b68e367148186b07ce882973
Cyril Mongis <cyril.mongis@adesso.de>
2020-05-24 14:22:40 +0200

    Added ResultColumn so an Algorithm can produce several columns

---
+++ src/main/java/backend/entries/ResultColumn.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend.entries;
+
+import backend.serverqueries.NetMHCPan41Query;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ *
+ * @author cyrilmongis
+ */
+public class ResultColumn {
+    
+    
+    public final static String RANK_EL = "(%Rank EL)";
+    public final static String RANK_BA = "(%Rank BA)";
+    public final static String NM = "(nM)";
+    public final static String RANK = "(%Rank)";
+    
+    
+    
+    
+    final Algorithm algorithm;
+    
+    final String suffix;
+    
+    public ResultColumn(Algorithm algorithm) {
+        this(algorithm,"");
+    }
+    
+    public ResultColumn(Algorithm algorithm, String suffix) {
+        this.algorithm = algorithm;
+        this.suffix = suffix;
+    }
+    
+    public int orderScore() {
+        
+        int i = 0;
+        for (Algorithm algo :  Algorithm.values()) {
+            if(algorithm == algo) {
+                return i;
+            }
+            
+            i++;
+        }
+        return i;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        return hashCode() == object.hashCode();
+    }
+    
+    
+    public boolean isAlgorithmInList(String... servers) {
+       for(String server : servers) {
+
+           Algorithm algo = Algorithm.valueOf(server);  
+           if(algo == null) {
+               continue;
+           }
+           if(algo == algorithm) {
+               return true;
+           }
+       }
+       
+       return false;
+                
+    }
+    
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(algorithm.hashCode())
+                .append(suffix).toHashCode();
+    }
+    
+    @Override
+    public String toString() {
+        return new StringBuilder(20).append(algorithm.toString())
+                .append(" ")
+                .append(suffix)
+                .toString();
+    }
+   
+    
+}
---
+++ src/main/java/backend/entries/ResultColumns.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend.entries;
+
+/**
+ *
+ * @author cyrilmongis
+ */
+public class ResultColumns {
+    
+    
+    /* Requirements:
+    From left to right it should be:
+
+        NetMHCpan 4.1 (%Rank EL)
+        NetMHCpan 4.1 (%Rank BA)
+        NetMHCpan 4.1 (nM)
+
+        NetMHC 4.0 (%Rank)
+        NetMHC 4.0 (nM)
+
+        NetMHCpan 4.0 (%Rank)
+        NetMHCpan 4.0 (nM)
+
+        NetMHCpan 3.0 (%Rank)
+        NetMHCpan 3.0 (nM)
+
+        NetMHC 3.4 (%Rank)
+        NetMHC 3.4 (nM)
+
+        NetMHCcons 1.1  (nM)
+        Pickpocket 1.1  (nM)
+
+        IEDB Recommended (EL-Score) – better (%Rank)
+        IEDB Consensus (%Rank)
+        IEDB SMMPMBEC (nM)
+        IEDB SMM (nM)
+        SYFPEITHI (score)
+    */
+    
+    
+    public static final ResultColumn[] ALL = {
+        Algorithm.NetMHCpan41.toColumn(ResultColumn.RANK_EL),
+        //Algorithm.NetMHCpan41.toColumn(ResultColumn.RANK_BA),
+        Algorithm.NetMHCpan41.toColumn(),
+        
+        Algorithm.NetMHC40.toColumn(ResultColumn.RANK),
+        Algorithm.NetMHC40.toColumn(),
+        
+        Algorithm.NetMHCpan40.toColumn(ResultColumn.RANK),
+        Algorithm.NetMHCpan40.toColumn(),
+        
+        Algorithm.NetMHCpan30.toColumn(ResultColumn.RANK),
+        Algorithm.NetMHCpan30.toColumn(),
+        
+        //Algorithm.NetMHC34.toColumn(ResultColumn.RANK), // --> No Rank in this one
+        Algorithm.NetMHC34.toColumn(),
+        
+        Algorithm.NetMHCpan28.toColumn(),
+        
+        Algorithm.IEDB_netMHCcons.toColumn(),
+        Algorithm.IEDB_pickpocket.toColumn(),
+        Algorithm.IEDB_recommended.toColumn(),
+        Algorithm.IEDB_consensus.toColumn(),
+        Algorithm.IEDB_smmpmbec.toColumn(),
+        Algorithm.SYFPEITHI.toColumn()};
+
+}
